ebb37e561aee95a0577626402cd6dd435b98fa62
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-03-28 13:56:47 +0200

    use cross-fetch

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import fetch from 'node-fetch';
+import fetch from 'cross-fetch';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('ftech-csvtocode.generateBindings', generateBindingsCommand);
