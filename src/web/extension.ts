0183244169caffae24d7826c221dffcca2327a4e
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-03-28 14:10:36 +0200

    use axios instead of fetch

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import fetch from 'cross-fetch';
+import axios from 'axios';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('ftech-csvtocode.generateBindings', generateBindingsCommand);
@@ -99,17 +99,8 @@ async function generateBindingsCommand() {
     const apiUrl = 'https://coder.farawaytech.com/api/csvtocode';
 
     try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ csvContent })
-        });
-
-        if (!response.ok) {
-            throw new Error(`Error communicating with the Coder API: ${response.statusText}`);
-        }
-
-        return await response.text();
+		const response = await axios.post(apiUrl, { "csvContent": csvContent });
+        return response.data;
     } catch (error) {
         vscode.window.showErrorMessage(`Failed to process CSV: ${error}`);
         throw error;
