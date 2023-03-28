d6bb28730757ff7cf6454043cf63ef09a5bc6669
Roman Prokofyev <roman.prokofyev@gmail.com>
2023-03-28 13:45:28 +0200

    minor

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fetch from 'node-fetch';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('ftech-csvtocode.generateBindings', generateBindingsCommand);
